0c0a6a5104a911403053a2d9a797bda6b4b81e81
Drawinakash0209 <drawinakash77@gmail.com>
2024-03-13 13:44:43 +0530

    post-edit

--- apiit-blog/app/Http/Controllers/PostController.php
+++ apiit-blog/app/Http/Controllers/PostController.php
@@ -11,7 +11,8 @@ class PostController extends Controller
 {
     public function index()
     {
-        return view('post.index');
+        $posts = Post::all();
+        return view('post.index', compact('posts'));
     }
 
     public function create()
@@ -50,4 +51,41 @@ class PostController extends Controller
         return redirect('/post')->with('message', 'Post added successfully');
 
     }
+
+    public function edit($post_id)
+    {
+        $categories = Category::where('status','0')->get();
+        $post = Post::find($post_id);
+        return view('post.edit',compact('post', 'categories'));
+    }
+
+    public function update(PostFormRequest $request, $post_id)
+    {
+        $data = $request->validated();
+
+        $post = Post::find($post_id);
+        $post->category_id = $data['category_id'];
+        $post->name = $data['name'];
+        $post->slug = $data['description'];
+        $post->description = $data['description'];
+        $post->v_iframe = $data['v_iframe'];
+  
+        if ($request->hasFile('image')){
+            $file = $request->file('image');
+            $filename = time() . '.' . $file->getClientOriginalExtension();
+            $file->move('uploads/post/', $filename);
+            $post->image = $filename;
+    
+        }
+
+        $post->meta_title = $data['meta_title'];
+        $post->meta_description = $data['meta_description'];
+        $post->meta_keywords = $data['meta_keywords'];
+        $post->status = $request->status == true ? '1':'0';
+        $post->created_by = 0;//Auth::user()->id;
+        $post->update();
+
+        return redirect('/post')->with('message', 'Post updated successfully');
+
+}
 }
