f07229f6b0463b7f1dc60577a0b7cee2f7a40fcc
Drawinakash0209 <drawinakash77@gmail.com>
2024-03-21 14:41:17 +0530

    work

---
+++ resources/views/admin/category/edit.blade.php
@@ -0,0 +1,92 @@
+@extends('layout')
+
+@section('content')
+    <div class="container-fluid px-4">
+        
+ 
+
+    <div class="card mt-4">
+        <div class="card-header">
+             <h4 class="">Edit Category</h4>
+
+        </div>
+
+        <div class="card-body">
+
+           
+            @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+           
+            
+
+            <form action="{{url('/admin/update-category/'.$category->id)}}" method="POST" enctype="multipart/form-data">
+                @csrf
+                @method('PUT')
+
+                <div class="mb-3">
+                    <label for="category_name" class="form-label">Category Name</label>
+                    <input type="text" name="name" value="{{$category->name}}" class="form-control">
+                </div>
+
+                <div class="mb-3">
+                    <label for="">Slug</label>
+                    <input type="text" name="slug"value="{{$category->slug}}" class="form-control">
+                </div>
+
+                <div class="mb-3">
+                    <label for="description" class="form-label">Category Description</label>
+                    <textarea name="description" rows="5" class="form-control">{{$category->description}}</textarea>
+                </div>
+
+                <div class="mb-3">
+                    <label for="image" class="form-label">Image</label>
+                    <input type="file" name="image" class="form-control">
+                </div>
+
+                <h5>SEO Tags</h5>
+
+                <div class="mb-3">
+                    <label for="">Meta Title</label>
+                    <input type="text" name="meta_title" value="{{$category->meta_title}} class="form-control">
+                </div>
+
+                <div class="mb-3">
+                    <label for="description" class="form-label">Meta Description</label>
+                    <textarea name="meta_description" rows="3"  class="form-control">{{$category->meta_description}}</textarea>
+                </div>
+
+
+                <div class="mb-3">
+                    <label for="">Meta Keywords</label>
+                    <textarea name="meta_keywords" rows="3" class="form-control"> {{$category->meta_keywords}}</textarea>
+                </div>
+
+                <h6>Status Mode</h6>
+                <div class="row">
+                    <div class="col-md-3 mb-3">
+                        <label for="">Navbar Status</label>
+                        <input type="checkbox" name="navbar_status" {{$category->navbar_status == '1' ? 'checked' : ''}}>
+                    </div>
+
+                    <div class="col-md-3 mb-3">
+                        <label for="">Status</label>
+                        <input type="checkbox" name="status" {{$category->status == '1' ? 'checked' : ''}}>
+                    </div>
+
+                    <div class="col-md-6">
+                        <button  type="submit" class="btn-btn-primary">Submit</button>
+                    </div>
+                        
+
+               
+            
+    </div>
+</div>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,60 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\DashboardController;
+use App\Models\Post;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('user.home', [
+        'blog' => Post::latest()->get()
+    ]);
+});
+
+Route::get('/home/{post}', [PostController::class, 'show']);
+
+
+Route::prefix('admin')->group(function () {
+
+    //Admin Dashboard
+    Route::get('/dashboard', [DashboardController::class, 'index']);
+
+    Route::get('/categories', [CategoryController::class, 'index']);
+
+    Route::get('/add-category', [CategoryController::class, 'Create']);
+
+    Route::post('/add-category', [CategoryController::class, 'store']);
+
+    Route::get('/edit-category/{category_id}', [CategoryController::class, 'edit']);
+
+    Route::put('/update-category/{category_id}', [CategoryController::class, 'update']);
+
+    Route::get('/delete-category/{category_id}', [CategoryController::class, 'destroy']);
+
+});
+
+Route::get('/post',[PostController::class, 'index']);
+
+Route::get('/add-post',[PostController::class, 'create']);
+
+Route::post('/add-post',[PostController::class, 'store']);
+
+Route::get('/post-edit/{post_id}', [PostController::class, 'edit']);
+
+Route::put('update-post/{post_id}', [PostController::class, 'update']);
+
+Route::get('/post-delete/{post_id}', [PostController::class, 'destroy']);
+
+Route::get('/search', [PostController::class, 'search']);
