ecb9b776f0f3392ed9372c075c0009b769ff7c6d
Drawinakash0209 <drawinakash77@gmail.com>
2024-04-06 10:51:20 +0530

    survey-approval-done

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -89,44 +89,6 @@
 </div>
 
 
-
-
-{{-- <div class="text-gray-900 pt-12 pr-0 pb-14 pl-0 bg-white">
-    <div class="w-full pt-4 pr-5 pb-6 pl-5 mt-0 mr-auto mb-0 ml-auto space-y-5 sm:py-8 md:py-12 sm:space-y-8 md:space-y-16
-        max-w-7xl"> --}}
-{{-- <div class="flex flex-col items-center sm:px-5 md:flex-row">
-    <div class="flex flex-col items-start justify-center w-full h-full pt-6 pr-0 pb-6 pl-0 mb-6 md:mb-0 md:w-1/2">
-      <div class="flex flex-col items-start justify-center h-full space-y-3 transform md:pr-4 lg:pr-6
-          md:space-y-5">
-    
-        <a class="text-5xl font-bold leading-none lg:text-5xl xl:text-6xl">Unlocking Knowledge, Inspiring Excellence <br><span class="space-y-6">APIIT</span></a>
-       
-      </div>
-    </div>
-    <div class="w-full md:w-1/2">
-      <div class="block">
-        <img
-            src="user/images/graduation.jpg" class="object-cover rounded-lg max-h-64 sm:max-h-96 btn- w-full h-full"/>
-      </div>
-    </div>
-  </div> --}}
-
-  
-
-
-
-
-
-
-
-
-
-
-
-
-    {{-- </div>
-</div> --}}
-
 <section class="section wb">
     <div class="container">
         <div class="row">
