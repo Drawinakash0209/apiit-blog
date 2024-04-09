4e68332080ef18d0de2de43984651031f8474199
Drawinakash0209 <drawinakash77@gmail.com>
2024-04-09 20:35:26 +0530

    share-2

--- app/Http/Controllers/ShareButtonsController.php
+++ app/Http/Controllers/ShareButtonsController.php
@@ -9,31 +9,7 @@ use Illuminate\Http\Request;
 
 class ShareButtonsController extends Controller
 {
-    public function share(){
-
-        $data= [
-            'title' => 'Laravel Share Buttons',
-            'description' => 'This is a simple Laravel package to share content on social media',
-            'image' => 'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png'
-
-        ];
-
-        $shareButtons = \Share::page(
-            url('/blogpost'),
-            'Here is the text',
-        )
-        ->facebook()
-        ->twitter()
-        ->linkedin()
-        ->whatsapp()
-        ->pinterest()
-        ->reddit()
-        ->telegram();
-
-        return view('post.blogpost', compact('shareButtons'));
-
-
-    }
+   
         
 
 
