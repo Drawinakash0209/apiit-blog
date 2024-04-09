f1610061a1aa6e2fee398e5830b03e1def061dd7
Drawinakash0209 <drawinakash77@gmail.com>
2024-04-09 20:32:33 +0530

    share-1

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use App\Models\Category;
-use Jorenvh\Share\Share;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\PostFormRequest;
