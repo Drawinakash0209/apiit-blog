c07c9d106a142e59820eda695ed795c488d05e58
Sheedh Mashood <sheedh.mashood@gmail.com>
2024-04-16 18:16:48 +0530

    updated navbar.blade.php
    cleansed the code and made the home tag functional

--- resources/views/user/navbar.blade.php
+++ resources/views/user/navbar.blade.php
@@ -1,4 +1,3 @@
-
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
 <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
