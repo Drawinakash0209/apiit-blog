2bf7e8b3063b728aad19415ab4c7c0bb99a1ee59
Drawinakash0209 <drawinakash77@gmail.com>
2024-04-16 11:08:26 +0530

    Home-UI3

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -92,7 +92,7 @@
 
 <div class="flex justify-center items-center">
     <!--- more free and premium Tailwind CSS components at https://tailwinduikit.com/ --->
-    <div class="2xl:mx-auto 2xl:container lg:px-20 lg:py-4 md:py-12 md:px-6 py-9 px-4 w-96 sm:w-auto">
+    <div class="2xl:mx-auto 2xl:container lg:px-20 lg:py-0 md:py-12 md:px-6 py-9 px-4 w-96 sm:w-auto">
       <div role="main" class="flex flex-col items-center justify-center">
         <h1 class="text-4xl font-semibold leading-9 text-center text-gray-800 dark:text-gray-50">This Week Blogs</h1>
         <p class="text-base leading-normal text-center text-gray-600 dark:text-white mt-4 lg:w-1/2 md:w-10/12 w-11/12">If you're looking for random paragraphs, you've come to the right place. When a random word or a random sentence isn't quite enough</p>
