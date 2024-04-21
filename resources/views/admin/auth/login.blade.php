b3735ef377fdf94444944fa16e53848a4c9e52b0
Sheedh Mashood <sheedh.mashood@gmail.com>
2024-04-21 16:20:46 +0530

    Update login.blade.php
    removed student option in admin login

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -1,24 +1,7 @@
 <x-guest-layout>
     {{-- Heading called Login for admins alligned to center --}}
-
-
+    
     <x-slot name="header">
-        <div class="sm:hidden">
-            <label for="tabs" class="sr-only">Select your country</label>
-            <select id="tabs" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                <option>Admin</option>
-                <option>Student</option>
-            </select>
-        </div>
-        <ul class="hidden text-sm font-medium text-center text-gray-500 rounded-lg shadow sm:flex dark:divide-gray-700 dark:text-gray-400">
-            <li class="w-full focus-within:z-10">
-                <a href="#" class="inline-block w-full p-4 text-black bg-gray-100 border-r border-gray-200 dark:border-gray-700 rounded-s-lg focus:ring-4 focus:ring-blue-300 active focus:outline-none dark:bg-gray-700 " aria-current="page">Admin</a>
-            </li>
-            <li class="w-full focus-within:z-10">
-                <a href="{{route('student.login')}}" class="inline-block w-full p-4 bg-white border-r border-gray-200 dark:border-gray-700 hover:text-gray-700 hover:bg-gray-50 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700">student</a>
-            </li>
-        </ul>
-
         <h2 class="mt-4 font-semibold text-xl text-gray-800 leading-tight text-center">
             {{ __('Admin Login') }}
         </h2>
