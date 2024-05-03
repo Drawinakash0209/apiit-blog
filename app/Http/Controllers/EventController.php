20996ecc61e7a15f09d1ede2182ad04d4e6c4b43
Sheedh Mashood <sheedh.mashood@gmail.com>
2024-05-03 15:56:42 +0530

    added event files

---
+++ app/Http/Controllers/EventController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\EventFormRequest;
+use App\Models\Event;
+use Illuminate\Support\Facades\Auth;
+
+class EventController extends Controller
+{
+
+    //Display all events
+    public function index(){
+        return view('events.events-page', [
+            'events' => Event::all()
+        ]);
+    }
+
+    //Display all user posted events
+    public function manage(){
+        return view('events.index', [
+            'events' =>  Auth::user()->events
+        ]);
+    }
+
+
+    //Delete event
+    public function destroy($event_id){
+        $event = Event::find($event_id);
+        $event->delete();
+        return redirect('/manage')->with('message', 'Event deleted successfully');
+    }
+    //Display edit form
+    public function edit($event_id){
+        $event = Event::find($event_id);
+        return view('events.edit', compact('event'));
+    }
+
+    //Update event
+    public function update(EventFormRequest $request, $event_id){
+        $data = $request->validated();
+        $event = Event::find($event_id);
+        $event->name = $data['name'];
+        $event->description = $data['description'];
+        $event->start_date = $data['start_date'];
+        $event->end_date = $data['end_date'];
+        $event->type_of_event = $data['type_of_event'];
+        $event->location = $data['location'];
+        $event->start_time = $data['start_time'];
+
+        if ($request->hasFile('image')){
+            $file = $request->file('image');
+            $filename = time() . '.' . $file->getClientOriginalExtension();
+            $file->move('uploads/event/', $filename);
+            $event->image = $filename;
+
+        }
+
+
+        $event->update();
+        return redirect('/manage')->with('message', 'Event updated successfully');}
+
+
+    //Display event creation page
+    public function create()
+    {
+        return view('events.create');
+    }
+
+    public function store(EventFormRequest $request){
+
+
+        $data = $request->validated();
+        $event = new Event;
+        $event->name = $data['name'];
+        $event->description = $data['description'];
+        $event->start_date = $data['start_date'];
+        $event->end_date = $data['end_date'];
+        $event->type_of_event = $data['type_of_event'];
+        $event->location = $data['location'];
+        $event->start_time = $data['start_time'];
+
+        if ($request->hasFile('image')){
+            $file = $request->file('image');
+            $filename = time() . '.' . $file->getClientOriginalExtension();
+            $file->move('uploads/event/', $filename);
+            $event->image = $filename;
+
+        }
+
+
+        $event->created_by = Auth::user()->id;
+
+        $event->save();
+        return redirect('/manage')->with('message', 'Post added successfully');
+
+
+    }
+
+    //Edit function (show form to edit event)
+
+
+}
---
+++ resources/views/components/events-cards.blade.php
@@ -0,0 +1,25 @@
+@props(['event'])
+<div class="relative w-full flex items-end justify-start text-left bg-cover bg-center" style="height: 450px; background-image:url({{$event->image ? asset('/uploads/event/' . $event->image): asset('/images/CR7.png')}});">
+    <div class="absolute top-0  right-0 bottom-0 left-0 bg-gradient-to-b from-transparent to-gray-900">
+    </div>
+
+    <div class="absolute top-0 right-0 left-0 mx-3 mt-2 flex justify-between items-center">
+        <a href="#" class="text-xs bg-indigo-600 text-white px-5 py-2 uppercase hover:bg-white hover:text-indigo-600 transition ease-in-out duration-500">{{$event['type_of_event']}}</a>
+        <span class="ml-2"></span>
+        <div class="text-white font-regular flex flex-col justify-start">
+            <span class="text-3xl leading-0 font-semibold"><?php echo date('d', strtotime($event['start_date'])); ?></span>
+            <span class="-mt-3"><?php echo date('M', strtotime($event['start_date'])); ?></span>
+        </div>
+    </div>
+
+    <main class="p-5 z-10">
+        <a href="#"
+           class="text-md tracking-tight font-medium leading-7 font-regular text-white hover:underline">{{$event['name']}}
+        </a>
+        <br>
+        <a href="#"
+           class="text-md tracking-tight font-medium leading-7 font-regular text-white hover:underline">{{$event['location']}}
+        </a>
+    </main>
+
+</div>
