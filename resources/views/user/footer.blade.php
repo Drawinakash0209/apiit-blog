dede23df82408fed1c4150bf6ab1defcf50fbc09
Sheedh Mashood <sheedh.mashood@gmail.com>
2024-05-03 14:39:12 +0530

    updated footer styles

--- resources/views/user/footer.blade.php
+++ resources/views/user/footer.blade.php
@@ -1,46 +1,45 @@
-<footer class="footer bg-gray-900 py-14">
-    <div class="container mx-auto">
-        <div class="grid grid-cols-1 md:grid-cols-4 gap-8">
-            <div class="footer-col">
-                <h4 class="text-white text-xl font-semibold mb-5">Quick Links</h4>
-                <ul>
-                    <li><a href="/" class="text-gray-400 hover:text-white transition duration-300">Home</a></li>
-                    <li><a href="/view-survey" class="text-gray-400 hover:text-white transition duration-300">Surveys</a></li>
-                    <li><a href="#" class="text-gray-400 hover:text-white transition duration-300">Feedback</a></li>
-                    <li><a href="#" class="text-gray-400 hover:text-white transition duration-300">Career Portal</a></li>
-                    <li><a href="#" class="text-gray-400 hover:text-white transition duration-300">About</a></li>
-                    <li><a href="#" class="text-gray-400 hover:text-white transition duration-300">Contacts</a></li>
-                </ul>
-            </div>
-            <div class="footer-col">
-                <h4 class="text-white text-xl font-semibold mb-5">Affiliated Universities</h4>
-                <ul>
-                    <li><a href="#" class="text-gray-400 hover:text-white transition duration-300">Staffordshire University</a></li>
-                    <li><a href="#" class="text-gray-400 hover:text-white transition duration-300">Deakin University</a></li>
-                    <li><a href="#" class="text-gray-400 hover:text-white transition duration-300">Asian Institute of Technology</a></li>
-                    <li><a href="#" class="text-gray-400 hover:text-white transition duration-300">NCUK</a></li>
-                    <li><a href="#" class="text-gray-400 hover:text-white transition duration-300">ICAEW</a></li>
-                    <li><a href="#" class="text-gray-400 hover:text-white transition duration-300">Southern Cross University</a></li>
-                    <li><a href="#" class="text-gray-400 hover:text-white transition duration-300">Western Sydney University</a></li>
-                </ul>
-            </div>
-            <div class="footer-col">
-                <h4 class="text-white text-xl font-semibold mb-5">Schools</h4>
-                <ul>
-                    <li><a href="#" class="text-gray-400 hover:text-white transition duration-300">IT School</a></li>
-                    <li><a href="#" class="text-gray-400 hover:text-white transition duration-300">Business School</a></li>
-                    <li><a href="#" class="text-gray-400 hover:text-white transition duration-300">Law School</a></li>
-                </ul>
-            </div>
-            <div class="footer-col">
-                <h4 class="text-white text-xl font-semibold mb-5">follow us</h4>
-                <div class="social-links flex space-x-4">
-                    <a href="#" class="inline-block h-10 w-10 bg-black-50 hover:bg-black hover:text-green-500 text-white rounded-full flex items-center justify-center transition duration-300"><i class="fab fa-facebook-f"></i></a>
-                    <a href="#" class="inline-block h-10 w-10 bg-black-50 hover:bg-black hover:text-green-500 text-white rounded-full flex items-center justify-center transition duration-300"><i class="fab fa-twitter"></i></a>
-                    <a href="#" class="inline-block h-10 w-10 bg-black-50 hover:bg-black hover:text-green-500 text-white rounded-full flex items-center justify-center transition duration-300"><i class="fab fa-instagram"></i></a>
-                    <a href="#" class="inline-block h-10 w-10 bg-black-50 hover:bg-black hover:text-green-500 text-white rounded-full flex items-center justify-center transition duration-300"><i class="fab fa-linkedin"></i></a>
-                </div>
+<footer class="footer">
+<div class="container">
+    <div class="row">
+        <div class="footer-col">
+            <h4>Quick Links</h4>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/view-survey">Surveys</a></li>
+                <li><a href="#">Life@APIIT</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contacts</a></li>
+            </ul>
+        </div>
+        <div class="footer-col">
+            <h4>Affiliated Universities</h4>
+            <ul>
+                <li><a href="#">Staffordshire University</a></li>
+                <li><a href="#">Deakin University</a></li>
+                <li><a href="#">Asian Institute of Technology</a></li>
+                <li><a href="#">NCUK</a></li>
+                <li><a href="#">ICAEW</a></li>
+                <li><a href="#">Southern Cross University</a></li>
+                <li><a href="#">Western Sydney University</a></li>
+            </ul>
+        </div>
+        <div class="footer-col">
+            <h4>Schools</h4>
+            <ul>
+                <li><a href="#">IT School</a></li>
+                <li><a href="#">Business School</a></li>
+                <li><a href="#">Law School</a></li>
+            </ul>
+        </div>
+        <div class="footer-col">
+            <h4>follow us</h4>
+            <div class="social-links">
+                <a href="#"><i class="fab fa-facebook-f"></i></a>
+                <a href="#"><i class="fab fa-twitter"></i></a>
+                <a href="#"><i class="fab fa-instagram"></i></a>
+                <a href="#"><i class="fab fa-linkedin"></i></a>
             </div>
         </div>
     </div>
+</div>
 </footer>
