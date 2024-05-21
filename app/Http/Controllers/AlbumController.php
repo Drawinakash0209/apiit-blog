9b1eb770a4858b379047235704caabb9a8fa7415
Drawinakash0209 <drawinakash77@gmail.com>
2024-05-21 15:12:31 +0530

    album update -4

--- app/Http/Controllers/AlbumController.php
+++ app/Http/Controllers/AlbumController.php
@@ -42,6 +42,35 @@ class AlbumController extends Controller
         ]);
     }
 
+    //function to update
+    public function update(Request $request, $id)
+    {
+        $formFields = $request->validate([
+            'name' => 'required',
+            'description' => 'required',
+            'gallery.*' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:5120'
+        ]);
+
+        $gallery = array();
+        if($files = $request->file('gallery')){
+            foreach($files as $file){
+                $galley_name = md5(rand(1000,10000));
+                $ext = strtolower($file->getClientOriginalExtension());
+                $gallery_full_name = $galley_name.'.'.$ext;
+                $upload_path = 'uploads/album/';
+                $gallery_url = $upload_path.$gallery_full_name;
+                $file->move($upload_path, $gallery_full_name );
+                $gallery[] = $gallery_url;
+            }
+        }
+        $formFields['gallery'] = implode('|', $gallery);
+        $formFields['created_by'] = auth()->id();
+
+        Album::where('id', $id)->update($formFields);
+
+        return redirect('/album');
+    }
+
     public function store(Request $request)
     {
         $formFields = $request->validate([
